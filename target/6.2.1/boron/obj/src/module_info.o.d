3451c5c61a1e459a2f356656b6e9296808685cc9
willnicewonger <wnicewonger@ucar.edu>
2025-04-11 09:46:39 -0600

    Add files via upload

---
+++ target/6.2.1/boron/obj/src/module_info.o.d
@@ -0,0 +1,39 @@
+/Users/wnicewonger/Downloads/3D-PAWS-Particle-FullStation-master/target/6.2.1/boron//obj/./src/module_info.o: \
+ src/module_info.c ../../../dynalib/inc/module_info.inc \
+ ../../../dynalib/inc/module_info.h ../../../services/inc/static_assert.h \
+ /Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/assert.h \
+ /Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_ansi.h \
+ /Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/newlib-nano/newlib.h \
+ /Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_newlib_version.h \
+ /Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/config.h \
+ /Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/ieeefp.h \
+ /Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/features.h \
+ /Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stddef.h \
+ /Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stdint.h \
+ /Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/stdint.h \
+ /Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_default_types.h \
+ /Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_intsup.h \
+ /Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_stdint.h \
+ ../../../hal/inc/hal_platform.h ../../../hal/shared/platforms.h \
+ ../../../hal/src/boron/hal_platform_config.h \
+ ../../../hal/src/nRF52840/hal_platform_nrf52840_config.h
+../../../dynalib/inc/module_info.inc:
+../../../dynalib/inc/module_info.h:
+../../../services/inc/static_assert.h:
+/Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/assert.h:
+/Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_ansi.h:
+/Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/newlib-nano/newlib.h:
+/Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_newlib_version.h:
+/Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/config.h:
+/Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/ieeefp.h:
+/Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/features.h:
+/Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stddef.h:
+/Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stdint.h:
+/Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/stdint.h:
+/Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_default_types.h:
+/Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_intsup.h:
+/Users/wnicewonger/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_stdint.h:
+../../../hal/inc/hal_platform.h:
+../../../hal/shared/platforms.h:
+../../../hal/src/boron/hal_platform_config.h:
+../../../hal/src/nRF52840/hal_platform_nrf52840_config.h:
